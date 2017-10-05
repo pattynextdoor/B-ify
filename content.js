eefc8f973ee76e8bf217135626348643af90939b
Patrick Tumbucon <ptumb001@ucr.edu>
2017-10-05 13:34:13 -0700

    Added new regex expression and fixed spacing

--- content.js
+++ content.js
@@ -15,7 +15,7 @@ for (var i = 0; i < elements.length; i++) {
         //if plain text
         if (node.nodeType === 3) {
             var text = node.nodeValue;
-            var replacedText = text.replace(/\sb/gi || /B/gi," " +  String.fromCodePoint(0x1F171));
+            var replacedText = text.replace(/\sb/g," " +  String.fromCodePoint(0x1F171)).replace(/B/g, String.fromCodePoint(0x1F171));
 
             if (replacedText !== text) {
                 element.replaceChild(document.createTextNode(replacedText), node);
