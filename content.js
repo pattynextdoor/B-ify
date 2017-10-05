f38149877607291f183b3b8bcc617e7740001a43
Patrick Tumbucon <ptumb001@ucr.edu>
2017-10-05 12:06:18 -0700

    Added regex

--- content.js
+++ content.js
@@ -1,4 +1,10 @@
 var elements = document.getElementsByTagName('*');
+var Bstring = "<i class=\"em em-b\"></i>";
+var Bobject = document.createElement('i');
+Bobject.innerHTML = Bstring;
+var htmlObject = Bobject.firstChild;
+
+document.body.innerHTML+= "<link href= \"https://afeld.github.io/emoji-css/emoji.css\" rel=\"stylesheet\">";
 
 for (var i = 0; i < elements.length; i++) {
     var element = elements[i];
@@ -6,9 +12,10 @@ for (var i = 0; i < elements.length; i++) {
     for (var j = 0; j < element.childNodes.length; j++) {
         var node = element.childNodes[j];
 
+        //if plain text
         if (node.nodeType === 3) {
             var text = node.nodeValue;
-            var replacedText = text.replace(/[word or phrase to replace here]/gi, '[new word or phrase]');
+            var replacedText = text.replace(/\sb/gi, htmlObject);
 
             if (replacedText !== text) {
                 element.replaceChild(document.createTextNode(replacedText), node);
